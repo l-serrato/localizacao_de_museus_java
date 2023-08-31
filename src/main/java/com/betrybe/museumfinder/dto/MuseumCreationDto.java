b8a0e03af55cf5fd3f0a8c2f6759d6bf43ea9fab
Luis <l-serrato@live.com>
2023-08-31 16:38:45 -0300

    1testing

--- src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
@@ -1,10 +1,18 @@
 package com.betrybe.museumfinder.dto;
 
 import com.betrybe.museumfinder.model.Coordinate;
+
 /**
  * Record.
  */
-public record
-MuseumCreationDto(String name, String description, String address,
-                                String collectionType, String subject, String url, Coordinate coordinate) {
+public record MuseumCreationDto(
+    String name,
+    String description,
+    String address,
+    String collectionType,
+    String subject,
+    String url,
+    Coordinate coordinate
+) {
+
 }
