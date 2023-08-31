6ea91dc9c76cabc666d469e945291bbd06875347
Luis <l-serrato@live.com>
2023-08-31 19:29:10 -0300

    3testing

--- src/main/java/com/betrybe/museumfinder/service/MuseumService.java
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -2,7 +2,6 @@ package com.betrybe.museumfinder.service;
 
 import com.betrybe.museumfinder.database.MuseumFakeDatabase;
 import com.betrybe.museumfinder.exception.InvalidCoordinateException;
-import com.betrybe.museumfinder.exception.MuseumNotFoundException;
 import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.util.CoordinateUtil;
@@ -11,45 +10,41 @@ import org.springframework.stereotype.Service;
 /**
  * Class.
  */
+
 @Service
 public class MuseumService implements MuseumServiceInterface {
-  MuseumFakeDatabase museumFakeDatabase;
-  public MuseumService(MuseumFakeDatabase museumFakeDatabase) {
 
-    this.museumFakeDatabase = museumFakeDatabase;
+  MuseumFakeDatabase museumDatabase;
+
+  /**
+   * Constructor.
+   */
+  public MuseumService(MuseumFakeDatabase museumDatabase) {
+    this.museumDatabase = museumDatabase;
   }
+
   @Override
   public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
-
-    double lon = coordinate.longitude();
-    double lat = coordinate.latitude();
-
-    Coordinate coor = new Coordinate(lat, lon);
-
-    if (!CoordinateUtil.isCoordinateValid(coor)) {
-      throw  new InvalidCoordinateException();
-    }
-
-    return museumFakeDatabase.getClosestMuseum(coordinate, maxDistance)
-        .orElseThrow(MuseumNotFoundException::new);
+    return null;
   }
 
   @Override
   public Museum createMuseum(Museum museum) {
-    double lon = museum.getCoordinate().longitude();
     double lat = museum.getCoordinate().latitude();
-
-    Coordinate coor = new Coordinate(lat, lon);
-
-    if (!CoordinateUtil.isCoordinateValid(coor)) {
-      throw  new InvalidCoordinateException();
-    }
-
-    return museumFakeDatabase.saveMuseum(museum);
+    double lon = museum.getCoordinate().longitude();
+    isCoordinatesValid(lat, lon);
+    return museumDatabase.saveMuseum(museum);
   }
 
   @Override
   public Museum getMuseum(Long id) {
     return null;
+  }
+
+  private static void isCoordinatesValid(double lat, double lon) {
+    Coordinate coordinate = new Coordinate(lat, lon);
+    if (!CoordinateUtil.isCoordinateValid(coordinate)) {
+      throw new InvalidCoordinateException();
+    }
   }
 }
